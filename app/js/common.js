e22294a1d26aed8ba438298e9ebef4f1d0e45416
CollectBrains <ruzanow.dmitry@gmail.com>
2018-06-28 12:44:38 +0300

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,18 @@
 $(function() {
 
-	$(".js-slide").slick();
+	$(".js-slide").slick({
+		prevArrow: '.slider__prev',
+		nextArrow: '.slider__next'
+	});
+
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
 
 	svg4everybody();
 
